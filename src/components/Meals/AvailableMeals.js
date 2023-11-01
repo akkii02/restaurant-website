18e22ca33c60bf5f52839b81a14e7d279073b778
akshaysable02 <akshaysable097@gmail.com>
2023-11-01 20:41:27 +0530

    Making a hard Coded Cart without any Functionality

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,65 @@
+import React from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card"
+import MealItem from "./MealItem/MealItem";
+
+
+const indianFoodMenu = [
+      {
+        id: 1,
+        foodname: "Palak Paneer",
+        description:
+          "Creamy spinach curry with cubes of paneer (Indian cottage cheese) and aromatic spices.",
+        price: 180,
+      },
+      {
+        id: 2,
+        foodname: "Aloo Gobi",
+        description:
+          "A dry dish made with potatoes (aloo) and cauliflower (gobi), cooked with onions, tomatoes, and spices.",
+        price: 90,
+      },
+      {
+        id: 3,
+        foodname: "Dal Tadka",
+        description:
+          "Yellow lentils (dal) cooked and tempered with mustard seeds, cumin, garlic, onions, and tomatoes.",
+        price: 8.49,
+      },
+      {
+        id: 4,
+        foodname: "Vegetable Biryani",
+        description:
+          "Fragrant rice dish cooked with mixed vegetables and biryani spices, served with raita.",
+        price: 250,
+      },
+      {
+        id: 5,
+        foodname: "Chana Masala",
+        description:
+          "Spicy and tangy chickpea curry cooked with onions, tomatoes, and a blend of Indian spices.",
+        price: 100,
+      },
+    ];
+    
+
+function AvailableMeals() {
+  const mealsList = indianFoodMenu.map((meal) => (
+    <MealItem
+    id={meal.id}
+      key={meal.id}
+      name={meal.foodname}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+}
+
+export default AvailableMeals;
